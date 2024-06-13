68a9f374da9485b2d3d578107d3c919f30eae816
Dhiraj2060 <dhirajsharma2060@gmail.com>
2024-06-13 14:45:17 +0530

    replication first test

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -6,3 +6,11 @@
 #
 # DON'T EDIT THIS!
 exec python3 -m app.main "$@"
+#!/bin/bash
+
+# Ensure the correct virtual environment is activated, if using one
+# source /path/to/your/venv/bin/activate
+
+# Run the Python script with the provided arguments
+python main.py "$@"
+
